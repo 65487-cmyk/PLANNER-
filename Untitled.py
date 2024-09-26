b8b0737557f586f96a1b3fefa4e4aa9a66e4f955
65487-cmyk <vaseyvarren@gmail.com>
2024-09-26 14:05:24 +0300

    reviewed planner

---
+++ Untitled.py
@@ -0,0 +1,19 @@
+
+def calculate(x,y):
+     return (x,y)
+x=4
+y=5
+calculate(x,y)
+def add():
+     print(x+y)
+add(calculate(x,y))
+
+    
+def subtract(x,y):
+     print(x-y)
+    
+
+    
+
+
+
--- reviewed planner.py
+++ reviewed planner.py
@@ -6,32 +6,61 @@ print('who are you? ')
  #ask the user to input name
 name=input().lower()
 if name == 'vee':
-    print(f'welcome {name}, whats the password?')
+    print(f'welcome {name}')
 
 elif name!='vee':
-    print('unidentified username')
+    print('unidentified username try again')
+
+    
     
 #number of turns to input correct password
 max_trial=3
 turn_trial=0
-while turn_trial <max_trial:
-    password=input().capitalize().strip()
+while turn_trial < max_trial:
+    password=input('what is the password?: ').capitalize().strip()
     if password == 'Bananas':
-
         print('acccess granted')
         break
     else:
-        print('wrong password!Try again')
+        print('wrong password')
     turn_trial +=1
-    if turn_trial == max_trial:
-        print('access denied!')
+    continue
+if turn_trial == max_trial:
+    print('access denied!')
+    exit()
+
 #outlay of financial planner
 #finacial calculator
 #testing git
-#ask vee to input the total income to be budgeted for
-total_income=()
-def c_income(total_income):
-    x=(input('enter your average income'))
-    total_income=int(x)
-    value=c_income(total_income)
-    return'your average income is {value} kes'
+#ask vee to input the total income
+def total_income():
+    x=input('enter your total income \n')
+    cash=int(x)
+    return cash
+
+def expense():
+    t_expense=(50/100 * total_income())
+    print (f'Your total budget for expenses is {t_expense}')
+expense()
+d={}
+numbers=int(input('enter the total number of categories: '))
+for number in range(numbers):
+    expenditure=input('enter the expenditure category: ')
+    amount=int(input('enter the amount to cater for the specific expenditure: '))
+    d[expenditure]=amount
+print(d)
+print(f'The total amount spent on expenses is equal to {sum(d.values())}')
+
+if sum(d.values()) <= expense():
+    print('you are within the suitable range')
+
+
+
+
+
+
+
+
+
+   
+    
